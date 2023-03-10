1c0bc4126618c65d329838d408d0ddd082bfa882
6ahamed9 <ahamedmonir621@gmail.com>
2023-03-09 21:42:58 -0500

    Practice

---
+++ src/sports/Cricket.java
@@ -0,0 +1,16 @@
+package sports;
+
+public class Cricket {
+
+	public static void main(String[] args) {
+		
+		String name = "Mashrafe";
+		int age = 45;
+		
+		System.out.println("Player name is: "+name+ "\n" + "Player age is: "+age);
+		
+		
+
+	}
+
+}
